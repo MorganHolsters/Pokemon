6fa7b73a8882f3749d97204eae03707bc4d485d3
florthas <64018493+florthas@users.noreply.github.com>
2020-05-07 11:21:02 +0200

    Update sp_getPokemon_Florian.sql

--- Backend/scriptProcedures/sp_getPokemon_Florian.sql
+++ Backend/scriptProcedures/sp_getPokemon_Florian.sql
@@ -1,6 +1,5 @@
-CREATE PROCEDURE "DBA"."p_getPokemon" (in pid tinyint)
+CREATE PROCEDURE "DBA"."p_getPokemon" ()
 BEGIN  
     select DBA.pokemon.nom_pokemon, DBA.pokemon.pv_totaux, DBA.pokemon.sexe_pokemon
         from pokemon
-     where id_pokemon = pid
 end;

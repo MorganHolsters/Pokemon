7d9ba5da9b5f5a71686efd199376c4bc1cbd0c9d
MorganHolsters <64076952+MorganHolsters@users.noreply.github.com>
2020-05-08 14:06:57 +0200

    Update sp_http_getIMG.sql

--- Backend/scriptProcedures/sp_http_getIMG.sql
+++ Backend/scriptProcedures/sp_http_getIMG.sql
@@ -1,9 +1,7 @@
 CREATE PROCEDURE "DBA"."http_getIMG"(in url char(255))
 // renvoie le contenu de l image
 BEGIN
---
   call sa_set_http_header('Content-Type', 'image/png'); 
     Call sa_set_http_header('Access-Control-Allow-Origin', '*'); 
     select xp_read_file(dba.getPath() || 'img\' || url);  
---
 END;

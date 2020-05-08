8606145727c527f96a9724c438689a22f1600043
MorganHolsters <64076952+MorganHolsters@users.noreply.github.com>
2020-05-08 14:07:50 +0200

    Update sp_http_getPage.sql

--- Backend/scriptProcedures/sp_http_getPage.sql
+++ Backend/scriptProcedures/sp_http_getPage.sql
@@ -1,9 +1,7 @@
 CREATE PROCEDURE "DBA"."http_getPage"(in url char(255))
 // renvoie le contenu de la page html 
 BEGIN
---
     call sa_set_http_header('Content-Type', 'text/html; charset=utf-8'); 
     Call sa_set_http_header('Access-Control-Allow-Origin', '*'); 
     select xp_read_file(dba.getPath() || url || '.html'); 
--- 
 END;

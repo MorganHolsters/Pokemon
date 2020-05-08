4196b574e5ab3fb918e7b9a8536b4753630487a2
MorganHolsters <64076952+MorganHolsters@users.noreply.github.com>
2020-05-08 14:07:26 +0200

    Update sp_http_getJS.sql

--- Backend/scriptProcedures/sp_http_getJS.sql
+++ Backend/scriptProcedures/sp_http_getJS.sql
@@ -1,9 +1,7 @@
 CREATE PROCEDURE "DBA"."http_getJS"(in url char(255))
 // renvoie le contenu du script js 
 BEGIN
--- 
   call sa_set_http_header('Content-Type', 'application/javascript'); 
     Call sa_set_http_header('Access-Control-Allow-Origin', '*'); 
     select xp_read_file(dba.getPath() || 'js\' || url);           
---
 END;

4c4a40a6ea4168210568ecfb936e2d1339966542
MorganHolsters <he201896@students.ephec.be>
2020-05-08 14:57:46 +0200

    changes to login page

--- Front-end/js/scores.js
+++ Front-end/js/scores.js
@@ -10,15 +10,15 @@ let xhr = new XMLHttpRequest();
 
 function initPage(){
     let tableauComplet = "";
-    console.log(donnees);
-    
     for(let i = 0;i<donnees.length && i<10; i++){
         tableauComplet += "<tr><td>" + "#" + (i+1) + "</td><td>" + donnees[i].user_name + "</td><td>" + donnees[i].victoire + "</td><td>" + donnees[i].defaite + "</td></tr>";
     }
-    
     document.getElementById("tableauScore").innerHTML = tableauComplet;
 }
 
 function retourPageAcceuil(){
     window.location = "index";
+}
+function rejouer(){
+    window.location = "login";
 }

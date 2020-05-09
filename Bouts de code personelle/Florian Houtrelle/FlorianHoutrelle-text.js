7d96472c0a156756811ead6bc9bb9f4f33fc47f5
florthas <64018493+florthas@users.noreply.github.com>
2020-05-09 15:21:03 +0200

    Update FlorianHoutrelle-text.js

--- Bouts de code personelle/Florian Houtrelle/FlorianHoutrelle-text.js
+++ Bouts de code personelle/Florian Houtrelle/FlorianHoutrelle-text.js
@@ -1,9 +1,10 @@
-    let texte2 = "<p>Les règles sont simples vous aurez accès à ##### et vous devrez en affrontez un nombre égal. Vous aurez accès à 4 attaques par pokémon chacune ayant une certaine chance de réussite… ou d’échec...</p>" +
-        "<p>Ainsi les chances de réussites par attaque seront classées comme suite: </p>" +
+    let texte2 = "<p>Les règles sont simples. Vous avez accès à ##### et vous devez en affronter un nombre égal. 4 attaques par Pokémon vous sont octroyées, chacune ayant un pourcentage de réussite… ou d’échec...</p>" +
+        "<p>Ainsi le taux de réussite par attaque est classé de la manière suivante: </p>" +
         "<p>- Attaque 1 : 80% de réussite.</p>" +
         "<p>- Attaque 2 : 70% de réussite.</p>" +
         "<p>- Attaque 3 : 60% de réussite.</p>" +
         "<p>- Attaque 4 : 50% de réussite.</p>" +
-        "<p>Sur ce si vous avez bien compris les règles il ne me reste que à vous souhaiter bonne chance.</p><input onclick=\"rotateSpeeches()\" type=\"button\" value=\"Suivant\" id=\"textChanger\">";
+        "<p>Sur ce, si vous avez bien compris les règles, il ne me reste qu’à vous souhaiter bonne chance !</p><input onclick=\"rotateSpeeches()\" type=\"button\" value=\"Suivant\" id=\"textChanger\">";
 
-    let texte3 = "<p>Bienvenue dans le monde des Pokémon ! <BR> Les Pokémon sont d’étranges créatures avec lesquelles certains d’entre nous se réunissent afin de les faire s’affronter au cours de formidables combats. <BR> Es-tu prêt pour l’aventure ? <br>Alors qu'attand-tu pour te présenter!</p><input onclick=\"rotateSpeeches()\" type=\"button\" value=\"Je me présente!\" id=\"textChanger\">";
+    let texte3 = "<p>Bienvenue dans le monde des Pokémon ! <BR> Les Pokémon sont d’étranges créatures avec lesquelles certains d’entre nous se réunissent dans le but d’organiser de formidables combats. <BR> Es-tu prêt pour l’aventure ? <br>Alors qu'attends-tu pour te présenter ?</p><input onclick=\"rotateSpeeches()\" type=\"button\" value=\"Je me présente!\" id=\"textChanger\">";
+
